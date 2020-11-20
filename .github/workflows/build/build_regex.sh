fad1516cd0b238b116510c64d2428c6acb2bfdbf
Samuel Stow <samuelstow@gmail.com>
2020-11-20 14:35:39 +1000

    update script

--- .github/workflows/build/build_regex.sh
+++ .github/workflows/build/build_regex.sh
@@ -10,6 +10,6 @@ export projects=()
 
 while IFS=  read -r -d $'\0'; do
     (echo $REPLY | grep -E "(kotlin|java)" | grep -E ${REGEX} )  && projects+=("$REPLY") && echo "Building ${REPLY}"
-done < <(find . -type'd' -depth 3 -print0)
+done < <(find . -type d -maxdepth 3 -mindepth 3 -print0)
 
 . .github/workflows/build/build_all.sh;
