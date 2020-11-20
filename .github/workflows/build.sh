3d07b92d3b8762e254f2eb7f67a170348e560668
Samuel Stow <samuelstow@gmail.com>
2020-11-19 17:05:30 -0800

    Update build.sh

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -2,8 +2,8 @@
 # Script to build project if changes occur that contain project dir or build script.
 
 CHANGES=$(git --no-pager diff --name-only $COMMIT_RANGE);
-echo "Changes: ${CHANGES}";
-if [[ -n "$(grep -E "(${PROJ_DIR}|\.github\/workflows\)" <<< "$CHANGES")" ]]; then
+echo "Changes: ${CHANGES}, Commit range: ${COMMIT_RANGE}, Project dir: ${PROJ_DIR}";
+if [[ -n "$(grep -E "(${PROJ_DIR}|\.github\/workflows)" <<< "$CHANGES")" ]]; then
   echo "Building for ${PROJ_DIR}";
   pushd "$PROJ_DIR";
   ./gradlew build;
