7bff02b9ac4b2c0e8d0c4cfd75d507c67e413b3d
Samuel Stow <samuelstow@gmail.com>
2020-11-23 08:54:47 +1000

    update scripts

--- .github/workflows/build/build.sh
+++ .github/workflows/build/build.sh
@@ -2,9 +2,9 @@
 # Script to build project if changes occur that contain project dir or build script.
 set -eo pipefail
 
-CHANGES=$(git --no-pager diff --name-only $COMMIT_RANGE);
+CHANGES="$(git --no-pager diff --name-only "${COMMIT_RANGE}")";
 echo "Commit range: ${COMMIT_RANGE}; Project dir: ${PROJ_DIR};\n Changes: ${CHANGES}";
-if [[ -n "$(grep -E "(${PROJ_DIR}|\.github\/workflows)" <<< $CHANGES)" ]]; then
+if [[ -n "$(grep -E "(${PROJ_DIR}|\.github\/workflows)" <<< "${CHANGES}")" ]]; then
   echo "Building for ${PROJ_DIR}";
   pushd "$PROJ_DIR";
   ./gradlew build;
--- .github/workflows/build/build_regex.sh
+++ .github/workflows/build/build_regex.sh
@@ -9,7 +9,7 @@ export COMMIT_RANGE="${COMMIT_RANGE}";
 export projects=()
 
 while IFS=  read -r -d $'\0'; do
-    (echo $REPLY | grep -E "(kotlin|java)" | grep -E ${REGEX} )  && projects+=("$REPLY") && echo "Building ${REPLY}"
+    (echo "${REPLY}" | grep -E "(kotlin|java)" | grep -E "${REGEX}" )  && projects+=("$REPLY") && echo "Building ${REPLY}"
 done < <(find . -type d -maxdepth 3 -mindepth 3 -print0)
 
 . .github/workflows/build/build_all.sh;
